7917f3a4ec9f8680b1e4018701420f0cb09059a8
keiko.sato <120654447+k0592239@users.noreply.github.com>
2023-01-08 22:29:24 +0900

    〜Lesson 7/Chapter 5.3：スプライトを表示する

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -12,6 +12,18 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         // 背景色を設定
         backgroundColor = UIColor(red: 0.15, green: 0.75, blue: 0.90, alpha: 1)
+        // 地面の画像を読み込む
+        let groundTexture = SKTexture(imageNamed: "ground")
+        groundTexture.filteringMode = .nearest
+        // テクスチャを指定してスプライをと作成する
+        let groudSprite = SKSpriteNode(texture:groundTexture)
+        // スプライトの表示する位置を指定する
+        groudSprite.position = CGPoint(
+            x: groundTexture.size().width / 2,
+            y: groundTexture.size().height / 2
+        )
+        // シーンにスプライをと追加する
+        addChild(groudSprite)
     }
 
 }
